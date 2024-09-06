338221fd84f8005fc684d62e3fa4fbac975edc5e
Himanshu Negi <himanegi2468@gmail.com>
2024-09-07 00:43:31 +0530

    define employee schema

---
+++ src/models/register.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const employeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contact: String,
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//hashed password
+employeeSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const Register = new mongoose.model("register", employeeSchema);
+module.exports = Register;
