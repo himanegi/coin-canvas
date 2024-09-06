b480c64116d2670aa92c34f348783f73f6fb60ed
Himanshu Negi <himanegi2468@gmail.com>
2024-09-07 00:42:38 +0530

    define schema for expense

---
+++ src/models/addExpense.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const expenseSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  paymentDate: String,
+  Description: String,
+  user: String,
+  paymentStatus: {
+    type: String,
+  },
+});
+
+const addExpense = new mongoose.model("expense", expenseSchema);
+
+module.exports = addExpense;
