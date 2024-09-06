ac0d8d570402da94a29b53970321a127424041a9
Himanshu Negi <himanegi2468@gmail.com>
2024-09-06 21:53:19 +0530

    import modules and fix package description

---
+++ src/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
+const notifier = require("node-notifier");
+const requests = require("requests");
+const port = process.env.PORT || 8000;
+require("./db/conn");
+
+const Registers = require("./model/register");
+const Expenses = require("./model/addExpense");
+const Supports = require("./model/support");
